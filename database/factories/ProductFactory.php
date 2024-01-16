ac4c69c05610d0f2d937d1bb79de9689632f7a2a
Ananda Bintang <anandabintang4@gmail.com>
2024-01-16 12:29:05 +0700

    added model, migration, factory, and seeder for many table

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'price' => $this->faker->numberBetween(100000, 10000000),
+            'description' => $this->faker->text,
+            'subcategory_id' => $this->faker->numberBetween(1, 15),
+        ];
+    }
+}
